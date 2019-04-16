23c8f911bc16d270d49bcfb63a3748588012b480
Trent Moss <trentmoss05@gmail.com>
2019-04-16 21:27:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = "Trent";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome,`
